3e9fda1dd31d8f0caa5b773994c5e8dfe5f71164
dhiaberrebah <mohameddhia.berrebah@ISAEG.u-gafsa.tn>
2025-01-29 20:40:50 +0100

    Revert "Update"
    This reverts commit 04e7412dd0a9e0204ddac056c36d8289ff693af5.

--- src/views/Index.js
+++ src/views/Index.js
@@ -14,14 +14,20 @@ export default function Index() {
           <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div className="pt-32 sm:pt-0">
               <h2 className="font-semibold text-4xl text-blueGray-600">
-              Donation TN 
+                Notus React - A beautiful extension for Tailwind CSS.
               </h2>
               <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-              Raise online money
-              for all occasions !
-                {" "}
-                
-                0% platform fees
+                Notus React is Free and Open Source. It does not change any of
+                the CSS from{" "}
+                <a
+                  href="https://tailwindcss.com/?ref=creativetim"
+                  className="text-blueGray-600"
+                  target="_blank"
+                >
+                  Tailwind CSS
+                </a>
+                . It features multiple HTML elements and it comes with dynamic
+                components for ReactJS, Vue and Angular.
               </p>
               <div className="mt-12">
                 <a
@@ -29,9 +35,15 @@ export default function Index() {
                   target="_blank"
                   className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
                 >
-                  Start a collect
+                  Get started
                 </a>
-                
+                <a
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                  className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                  target="_blank"
+                >
+                  Github Star
+                </a>
               </div>
             </div>
           </div>
