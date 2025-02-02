489d78ef05eff2af3033dca7365c2533a2132788
dhiaberrebah <mohameddhia.berrebah@ISAEG.u-gafsa.tn>
2025-02-02 17:23:55 +0100

    Update authentification and landing design

--- src/views/Index.js
+++ src/views/Index.js
@@ -31,7 +31,7 @@ export default function Index() {
               </p>
               <div className="mt-12">
                 <a
-                  href="http://localhost:3001/auth/login"
+                  href="http://localhost:3000/auth/login"
                   target="_blank"
                   className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
                 >
